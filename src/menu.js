9c26c0d75f62db08814b9f2025021331f85fc57b
jpikiel <jan.pikiel@biu.com.pl>
2022-11-03 19:57:07 +0100

    zmiana nazwy pliku questions file

---
+++ src/menu.js
@@ -0,0 +1,76 @@
+import './menu.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileLines } from "@fortawesome/free-solid-svg-icons";
+import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import {test, isLogged} from "./loginForms/loginForm"
+import { useNavigate } from "react-router-dom";
+import App from './App';
+import { isRegistered, test2 } from './loginForms/registerForm';
+
+const Menu = () => {
+
+  let navigate = useNavigate();
+
+
+  const Logged = () => {
+    return (
+      <div>
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet"/>
+
+  <div className="hello">
+<p className="anim">Hello, {isLogged? test : test2}</p></div>
+<div className="grid-container">
+
+    <div className="grid-item1">
+      <FontAwesomeIcon icon={faFileLines} />
+    <p>
+      Create a new exam
+    </p>
+  </div>
+    <div className="grid-item2">
+    <FontAwesomeIcon icon={faFolderOpen} />
+      <p>
+      Check your exams
+    </p>
+    
+  </div>
+    <div className="grid-item3">
+    <FontAwesomeIcon icon={faUsers} />
+      <p>
+      Join an exam
+    </p>
+    </div>
+    <div className="grid-item4"  onClick={()=>{
+      navigate("/");
+      }  
+      }>
+      <FontAwesomeIcon icon={faArrowRight} />
+      <p>Log Out</p>
+      </div>
+ 
+</div>
+
+</div>
+    )
+  }
+  if(isLogged){
+    return (
+      <Logged></Logged>
+      );
+  }else if (isRegistered){
+    return (
+      <Logged></Logged>
+      );
+  }else{
+    return (
+      <div className='notLoggedUser'>
+        Log in first!
+        <button onClick={() => {navigate("/")}}>Go back to login page</button>
+      </div>
+    )
+  }
+}
+ 
+export default Menu;
