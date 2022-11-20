ffe62991d81153d89ad0007e1f280a92e29ddd26
leilus <prettytigerr@gmail.com>
2022-11-20 18:39:31 +0100

    strona  z wybieraniem zestawu pytan

--- src/examJoin.js
+++ src/examJoin.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { isLogged } from "./loginForms/loginForm";
+import { isRegistered } from "./loginForms/registerForm";
+import NotLogged from "./errors/notLogged";
 import './examJoin.css';
 
-
 const ExamJoin = () => {
+    let navigate = useNavigate();
     return (
         <>
+            <button className="main" onClick={() => {navigate('/menu')}}>Main menu</button>
+
+
+        <div className="menu">
             <h1>Randomize quiz</h1>
             <h1>Join quiz</h1>
-        
+        </div>
         </>
 
 
       );
+
+    
 }
  
 export default ExamJoin;
