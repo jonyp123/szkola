db5d6929e7fb76b0ee2aba97bb799935a041c138
Jan Pikiel <j.pikiel@wp.pl>
2022-12-06 08:21:49 +0100

    join quiz i manage exams

--- src/ManageExams.js
+++ src/ManageExams.js
@@ -2,30 +2,46 @@ import NotLogged from "./errors/notLogged";
 import { useNavigate } from "react-router-dom";
 import { addDoc, collection, doc, getDoc, getDocs, onSnapshot, query, where } from "firebase/firestore";
 import {db} from './firebase-config';
+import { useEffect, useState } from "react";
 
 const ManageExams = () => {
     let navigate = useNavigate();
+    let path = "_document.data.value.mapValue.fields"
+    const usersCollectionRef = collection(db, "quiz")
+    const [quizes, setQuizes] = useState([])
+    const [quizesTable, setQuizesTable] = useState([])
+
+    useEffect(() => {
+        const getQuizes = async () => {
+          const data = await getDocs(usersCollectionRef);
+          setQuizes(data.docs); 
+        };
+        getQuizes();
+      }, [])
 
 
-const q = query(collection(db, "quiz").where('owner', '==', sessionStorage.getItem("nick")))
-const unsub = onSnapshot(q, (querySnapshot) => {
-  console.log("Data", querySnapshot.docs.map(d => doc.data()));
-});
+      const QuizList = 
+        quizes.length > 0 &&
+        quizes.map((item, i) =>{
+            console.log(i)
+            if(quizes[i]._document.data.value.mapValue.fields.owner.stringValue === sessionStorage.getItem("nick")){
+                return (
+                    <div key={i} style={{marginTop: 20}}>
+                        {quizes[i]._document.data.value.mapValue.fields.quizTitle.stringValue}
+                        <br></br>
+                        {quizes[i]._document.data.value.mapValue.fields.QuizPostCode.stringValue}
+                        <br></br>
+                    </div>
+            )
+            }else {
+                return(
+                    <div key={i}>
+                    
+                </div>
+                )
+            }
 
-// const getQuizes = async() =>  {
-//     let query = db.collection('quiz').where('owner', '==', sessionStorage.getItem("nick"));
-// try {
-//     const results = [];
-//     let querySnapshot = await query.get();
-//     querySnapshot.forEach(function(doc) {
-//        results.push(doc.data());               
-//     });
-//     return results;
-// } catch(e) {
-//     console.log('Error getting user: ', e);
-// }
-// }
-
+        })
 
 
 
@@ -43,7 +59,7 @@ const unsub = onSnapshot(q, (querySnapshot) => {
         return(
             <>
             <button className="main" onClick={() => {navigate('/menu')}}>Main menu</button>
-            <button className="main" onClick={() => {unsub()}}>pokaz dane</button>
+            {QuizList}
             </>
         )
     }else{
--- src/examJoin.js
+++ src/examJoin.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { isLogged } from "./loginForms/loginForm";
 import NotLogged from "./errors/notLogged";
 import './examJoin.css';
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+import { useForm } from "react-hook-form";
 
 const ExamJoin = () => {
     let navigate = useNavigate();
-    return (
-        <>
-            <button className="main" onClick={() => {navigate('/menu')}}>Main menu</button>
+    const usersCollectionRef = collection(db, "quiz") 
+    const [quizes, setQuizes] = useState([])
+    const [matchQuiz, setMatchQuiz] = useState()
+    const [showCodeInput, setShowCodeInput] = useState(false)
+    const {
+        register,
+        handleSubmit,
+        reset,
+        control,
+        watch,
+        setValue,
+        clearErrors,
+        formState: { errors },
+      } = useForm({ mode: "all" });
 
+    useEffect(() => {
+        const getQuizes = async () => {
+          const data = await getDocs(usersCollectionRef);
+          setQuizes(data.docs); 
+        };
+        getQuizes();
 
-        <div className="menu">
-            <h1>Randomize quiz</h1>
-            <h1>Join quiz</h1>
-        </div>
-        </>
+      }, [])
 
+      function chooseQuiz(e) {
+        for(let i = 0; i <= quizes.length; i++){
+            if(quizes[i]._document.data.value.mapValue.fields.QuizPostCode.stringValue === e.quizCode){
+                setMatchQuiz(quizes[i])
+                return 0;
+            }
+        }
+      }
 
-      );
+    if(sessionStorage.getItem("logged") === "true"){
+        return (
+            <>
+                <button className="main" onClick={() => {navigate('/menu')}}>Main menu</button>
+                <div className="menu">
+                    <h1>Randomize quiz</h1>
+                    <h1 onClick={() => {setShowCodeInput(true)}}>Join quiz</h1>
+                    {showCodeInput &&
+                    <form onSubmit={handleSubmit(chooseQuiz)}>
+                        <label>Quiz code</label>
+                        <input {...register('quizCode')}></input>
+                        <button>submit</button>
+                    </form> 
+                    }   
+                </div>
+            </>
+          );
+    }else{
+        return(
+            <NotLogged></NotLogged>
+        )
+    }
+
 
     
 }
